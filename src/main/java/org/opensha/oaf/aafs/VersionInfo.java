d6956bed2868b9cbba6c69bb3b1fa9258dbee04d
Michael Barall <gituser@invisiblesoft.com>
2021-11-06 14:15:28 -0700

    New forecast schedule

--- src/main/java/org/opensha/oaf/aafs/VersionInfo.java
+++ src/main/java/org/opensha/oaf/aafs/VersionInfo.java
@@ -13,7 +13,7 @@ public class VersionInfo  {
 
 	// Program version.
 
-	public static final String program_version = "Version 1.00.1372 (11/03/2021)";
+	public static final String program_version = "Version 1.00.1373 (11/06/2021)";
 
 	// Program sponsor.
 
@@ -29,7 +29,7 @@ public class VersionInfo  {
 
 	// Build.
 
-	public static final int build = 1372;
+	public static final int build = 1373;
 
 
 
