2278d00347b3dd8cf1a4778c52b3853af5818da0
Michael Barall <mbarall@usgs.gov>
2022-10-12 11:30:12 -0700

    Shadowing code updated for event-sequence product

--- src/main/java/org/opensha/oaf/aafs/VersionInfo.java
+++ src/main/java/org/opensha/oaf/aafs/VersionInfo.java
@@ -13,7 +13,7 @@ public class VersionInfo  {
 
 	// Program version.
 
-	public static final String program_version = "Version 1.00.1458 (05/27/2022)";
+	public static final String program_version = "Version 1.00.1459 (10/11/2022)";
 
 	// Program sponsor.
 
@@ -29,7 +29,7 @@ public class VersionInfo  {
 
 	// Build.
 
-	public static final int build = 1458;
+	public static final int build = 1459;
 
 
 
