3d9085a36c4d42a0694fa3934a4e44ec19428421
Michael Barall <mbarall@usgs.gov>
2023-02-16 11:06:00 -0800

    Minor update

--- src/main/java/org/opensha/oaf/aafs/VersionInfo.java
+++ src/main/java/org/opensha/oaf/aafs/VersionInfo.java
@@ -13,7 +13,7 @@ public class VersionInfo  {
 
 	// Program version.
 
-	public static final String program_version = "Version 1.00.1528 (02/07/2023)";
+	public static final String program_version = "Version 1.00.1529 (02/16/2023)";
 
 	// Program sponsor.
 
@@ -29,7 +29,7 @@ public class VersionInfo  {
 
 	// Build.
 
-	public static final int build = 1528;
+	public static final int build = 1529;
 
 
 
